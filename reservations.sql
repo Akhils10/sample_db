d6c4d7643b489a5b521c3d21e1da0b83711f8da2
Anmol Shrivastava <s.anmol1994@gmail.com>
2020-06-22 15:14:47 +0530

    Create reservations.sql

---
+++ reservations.sql
@@ -0,0 +1,57 @@
+CREATE TABLE `reservations` (
+  `id` mediumint,
+  `user_id` mediumint default NULL,
+  `reservation_timestamp_utc` int(11) default 0,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (1,4,1596589273),(2,5,1596024541);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (3,4,1596975314),(4,5,1596961489);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (5,3,1592393508),(6,4,1592552751);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (7,6,1597594909),(8,4,1593111020);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (9,6,1591526000),(10,4,1597235260);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (11,6,1594925366),(12,6,1596973100);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (13,3,1593208668),(14,2,1597884071);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (15,1,1596173998),(16,1,1596724914);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (17,1,1596888388),(18,6,1593645519);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (19,1,1597155514),(20,5,1593151486);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (21,2,1591968046),(22,4,1593826782);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (23,2,1592309813),(24,5,1591067171);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (25,5,1591329053),(26,2,1595989351);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (27,3,1594351086),(28,3,1594685530);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (29,3,1594590438),(30,6,1598167846);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (31,3,1596554085),(32,6,1594184365);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (33,6,1596179679),(34,6,1598511613);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (35,5,1597965564),(36,1,1592959197);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (37,5,1592169525),(38,5,1597840332);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (39,1,1598508175),(40,1,1592872332);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (41,5,1596690652),(42,4,1592056128);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (43,2,1596657842),(44,1,1596815688);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (45,3,1592929570),(46,1,1593231824);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (47,6,1598130258),(48,1,1595506489);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (49,3,1595579296),(50,1,1596237180);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (51,1,1592842063),(52,4,1591203625);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (53,3,1596857675),(54,1,1592939241);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (55,1,1595421641),(56,1,1598633605);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (57,6,1592388362),(58,6,1591159024);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (59,3,1597785990),(60,3,1596971534);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (61,2,1597472018),(62,3,1593944469);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (63,4,1598229845),(64,6,1593379067);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (65,6,1597930092),(66,2,1591132843);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (67,1,1598535420),(68,6,1597701319);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (69,1,1593047784),(70,3,1594115529);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (71,6,1593474480),(72,5,1591335442);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (73,5,1592674041),(74,2,1596834868);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (75,5,1591289555),(76,6,1594193730);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (77,3,1591009407),(78,1,1593091216);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (79,5,1595664807),(80,2,1597989991);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (81,6,1598367327),(82,4,1596017994);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (83,1,1597465785),(84,3,1594810202);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (85,3,1592469020),(86,3,1591880825);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (87,1,1598340130),(88,6,1597235811);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (89,1,1592300525),(90,4,1597221808);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (91,5,1591448313),(92,1,1594650725);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (93,1,1591929689),(94,5,1598614380);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (95,4,1595994349),(96,3,1593177752);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (97,1,1591170415),(98,4,1595614376);
+INSERT INTO `reservations` (`id`,`user_id`,`reservation_timestamp_utc`) VALUES (99,2,1596638084),(100,2,1596621710);
